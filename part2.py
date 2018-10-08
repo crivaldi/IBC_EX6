da67de2b181cf1aa310f7e0cb9b2edf922b1cde0
Patricia Portmann <pportman@nd.edu>
2018-10-08 15:42:55 -0400

    part2.py, first commit, all I did was import pandas more to come

---
+++ part2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  8 15:38:14 2018
+
+Exercise06 Part 2: uses the data from iris.csv to do a couple different tasks
+
+@author: Patricia
+"""
+
+import pandas
+
+
+
