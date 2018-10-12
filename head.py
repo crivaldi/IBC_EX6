afc512cc17dbd9b96b66a15b63323bb982a38e2a
Patricia Portmann <pportman@nd.edu>
2018-10-11 23:43:05 -0400

    Changed format of head.py into function that takes in string and integer.

--- head.py
+++ head.py
@@ -4,21 +4,15 @@ Created on Sat Oct  6 16:09:30 2018
 
 Patricia Portmann
 Exercise06 - Part 1
-Python code that replcated head functionality
+Python code that replicates head functionality
 
 @author: Patricia
-"""
-import numpy as np
+"""  
+# defines a function head
+def head (str, n) :
+    with open (str) as filename: # opens passed in string as filename
+        lines = [next(filename) for x in xrange(n)]
+    print(lines)
 
-filename = raw_input("What file are you using? ")
-print(type(filename))
-
-# an array variable assigned to the file's contents, genfromtxt allows for strings
-data=np.genfromtxt(filename,dtype='str')
-print(type(data))
-
-# variable to set equal to the umber of lines we want to pull from the top
-endline = input("How many lines do you want to print? ")
-print(type(endline))
-print(data[0:endline])
-# saves the data inside inflammation-01.csv into variable data
+head("iris.csv", 5)
+#calls the function, passes the string "iris.csv" and the number 5
